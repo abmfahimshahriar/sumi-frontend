1af0dbd9125e97e1b313c16dfd94cee98470f6d7
abmfahimshahriar <abmfahimshahriar@iut-dhaka.edu>
2021-03-24 10:40:05 +0600

    setup create sprint

--- src/components/Sprint/SprintList/SprintList.tsx
+++ src/components/Sprint/SprintList/SprintList.tsx
@@ -1,8 +1,8 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import "./SprintList.css";
-import { SprintListCard } from "../../../components";
-
+import { CreateProjectDialog, SprintListCard } from "../../../components";
+import { ProjectListCardSkeleton } from "../../../utility/components";
 // redux stuff
 import { connect } from "react-redux";
 import { getSprints } from "../../../store/actions/sprintActions";
@@ -11,6 +11,7 @@ import {
   SprintState,
   UIState,
 } from "../../../interfaces/GlobalTypes";
+import { Button } from "@material-ui/core";
 
 interface ParamTypes {
   projectId: string;
@@ -21,8 +22,18 @@ type Props = {
   sprint: SprintState;
   ui: UIState;
 };
-const SprintList: React.FC<Props> = ({ getSprints, sprint }) => {
+const SprintList: React.FC<Props> = ({ getSprints, sprint, ui }) => {
   const { projectId } = useParams<ParamTypes>();
+
+  const [open, setOpen] = useState(false);
+  const isUpdate = false;
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   useEffect(() => {
     getSprints(projectId);
@@ -32,10 +43,36 @@ const SprintList: React.FC<Props> = ({ getSprints, sprint }) => {
   const sprintsMarkup = sprint.sprints.map((item) => {
     return <SprintListCard key={item._id} sprint={item} />;
   });
+
+  const errorMarkup = <div>{sprint.sprintErrors[0]}</div>;
+  const noSprintMarkup = <div>There are no sprints under this project</div>;
+  const loadingCounter = [1, 2];
+  const loadingMarkup = loadingCounter.map((item: number) => (
+    <ProjectListCardSkeleton key={item} />
+  ));
   return (
     <div className="sprint-list-card">
       <h2>Sprints</h2>
-      {sprintsMarkup}
+      <div className="custom-header">
+        <Button variant="contained" color="primary" onClick={handleClickOpen}>
+          Create Sprint
+        </Button>
+        <CreateProjectDialog
+          open={open}
+          onClose={handleClose}
+          isUpdate={isUpdate}
+        />
+      </div>
+      {!sprint.hasSprintErrors &&
+        !ui.localLoading &&
+        sprint.sprints.length > 0 &&
+        sprintsMarkup}
+      {!sprint.hasSprintErrors &&
+        !ui.localLoading &&
+        sprint.sprints.length === 0 &&
+        noSprintMarkup}
+      {sprint.hasSprintErrors && !ui.localLoading && errorMarkup}
+      {ui.localLoading && loadingMarkup}
     </div>
   );
 };
