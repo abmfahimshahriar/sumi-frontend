96806ea02134169e2901f5295d77bf7e2b08165f
abmfahimshahriar <abmfahimshahriar@iut-dhaka.edu>
2021-04-03 17:15:43 +0600

    add gantt chart

--- src/components/Report/GanttChart/GanttChart.tsx
+++ src/components/Report/GanttChart/GanttChart.tsx
@@ -1,11 +1,86 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { connect } from "react-redux";
 import "./GanttChart.css";
-const GanttChart = () => {
-    return (
-        <div className="gantt-chart-card">
-            hello from gantt chart
-        </div>
-    )
-}
+import { getSprints } from "../../../store/actions/sprintActions";
+import {
+  ISprintMapStateToProps,
+  SprintState,
+} from "../../../interfaces/GlobalTypes";
+import { useParams } from "react-router-dom";
+import { Chart } from "react-google-charts";
 
-export default GanttChart;
+type Props = {
+  getSprints: Function;
+  sprint: SprintState;
+};
+
+interface ParamTypes {
+  projectId: string;
+  sprintId: string;
+}
+const GanttChart: React.FC<Props> = ({ getSprints, sprint }) => {
+  const { projectId } = useParams<ParamTypes>();
+
+  const [chartData, setChartData] = useState<any[]>([]);
+  useEffect(() => {
+    getSprints(projectId);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    makeChartData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [sprint.sprints]);
+
+  const makeChartData = () => {
+    const sprints = sprint.sprints;
+    const tempChartData = [];
+    const columns = [
+      { type: "string", label: "Task ID" },
+      { type: "string", label: "Task Name" },
+      { type: "date", label: "Start Date" },
+      { type: "date", label: "End Date" },
+      { type: "number", label: "Duration" },
+      { type: "number", label: "Percent Complete" },
+      { type: "string", label: "Dependencies" },
+    ];
+    tempChartData.push(columns);
+    for (let i = 0; i < sprints.length; i++) {
+      const item = sprints[i];
+      const chartDataItem = [];
+      let percentDone;
+      if(item.TotalStoryPoints === 0) percentDone = 0;
+      else percentDone = Math.round((item.CompletedStoryPoints/item.TotalStoryPoints) * 100);
+      chartDataItem.push(item._id);
+      chartDataItem.push(item.SprintName);
+      chartDataItem.push(new Date(item.StartDate));
+      chartDataItem.push(new Date(item.EndDate));
+      chartDataItem.push(null);
+      chartDataItem.push(percentDone);
+      chartDataItem.push(null);
+      tempChartData.push(chartDataItem);
+    }
+    setChartData(tempChartData);
+  };
+  return (
+    <div className="gantt-chart-card">
+      <Chart
+        width={"100%"}
+        chartType="Gantt"
+        loader={<div>Loading Chart</div>}
+        data={chartData}
+        rootProps={{ "data-testid": "1" }}
+      />
+    </div>
+  );
+};
+
+const mapStateToProps = (state: ISprintMapStateToProps) => ({
+  sprint: state.sprint,
+  ui: state.ui,
+});
+
+const mapActionToProps = {
+  getSprints,
+};
+export default connect(mapStateToProps, mapActionToProps)(GanttChart);
